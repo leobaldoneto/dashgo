708e73e781f0107bef5f17c78a0d9bf285f00fe4
Leobaldo Alantara Neto <leobaldoneto@gmail.com>
2022-11-22 13:04:48 -0300

    feat: Add links to buttons and navigation

--- src/pages/users/create.tsx
+++ src/pages/users/create.tsx
@@ -1,4 +1,5 @@
 import { Box, Button, Divider, Flex, Heading, HStack, SimpleGrid, VStack } from "@chakra-ui/react";
+import Link from "next/link";
 import Input from "../../components/Form/input";
 import Header from "../../components/Header";
 import Sidebar from "../../components/Sidebar";
@@ -26,10 +27,11 @@ export default function CreateUser() {
               <Input name='password_confirmation' type='password' label='Confirmação da senha' />
             </SimpleGrid>
           </VStack>
-          
           <Flex mt='8' justify='flex-end'>
             <HStack spacing='4'>
-              <Button colorScheme='whiteAlpha'>Cancelar</Button>
+              <Link href='/users'>
+                <Button as= 'a' colorScheme='whiteAlpha'>Cancelar</Button>
+              </Link>
               <Button colorScheme='pink'>Salvar</Button>
             </HStack>
           </Flex>
