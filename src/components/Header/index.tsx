1d003067322954f7ba9c843e5afac3e9cd5b84c9
Leobaldo Alantara Neto <leobaldoneto@gmail.com>
2022-11-22 12:31:56 -0300

    feat: Add responsive sidebar

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,4 +1,6 @@
-import { Flex, useBreakpointValue } from '@chakra-ui/react';
+import { Flex, Icon, IconButton, useBreakpointValue } from '@chakra-ui/react';
+import { RiMenuLine } from 'react-icons/ri';
+import { useSidebarDrawer } from '../../contexts/SidebarDrawerContext';
 
 import Logo from './Logo';
 import NotificationNav from './NotificationNav';
@@ -6,6 +8,7 @@ import Profile from './Profile';
 import SearchBox from './SearchBox';
 
 export default function Header() {
+  const { onOpen } = useSidebarDrawer();
   const isWideVersion = useBreakpointValue({
     base: false,
     lg: true
@@ -21,6 +24,18 @@ export default function Header() {
       px='6'
       align='center'
     >
+
+      {!isWideVersion && (
+        <IconButton 
+          aria-label='Abrir navegação'
+          icon={<Icon as={RiMenuLine} />}
+          fontSize='24'
+          variant='unstyled'
+          onClick={onOpen}
+          mr='2'
+        />
+      )}
+
       <Logo />
       { isWideVersion && <SearchBox /> }
         <Flex
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,15 @@
 import type { AppProps } from 'next/app';
 import { ChakraProvider } from '@chakra-ui/react';
+
+import { SidebarDrawerProvider } from '../contexts/SidebarDrawerContext';
 import { theme } from '../styles/theme';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={ theme } >
-      <Component {...pageProps} />
+      <SidebarDrawerProvider>
+        <Component {...pageProps} />
+      </SidebarDrawerProvider>
     </ChakraProvider>
     )
 }
